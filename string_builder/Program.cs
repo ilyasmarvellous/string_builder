150c6b62d1bd5309d594e7204f022409afb311b8
ilyas <ilyas@LAPTOP-NOQ3L7Q3>
2017-02-20 23:45:50 -0600

    Add project files.

---
+++ string_builder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            StringBuilder s = new StringBuilder("Hi");
+            string newstr = "Ilyas";
+            s.Append(newstr);
+            Console.WriteLine(s);
+            Console.ReadLine();
+
+        }
+    }
+}
